5a858f4318624940796117959f00c0aca5fed12d
nlhchinh <chinhnguyen1692000z@gmail.com>
2024-07-20 00:25:18 +0700

    push project

---
+++ src/main/java/com/chinh/AnimalGrowth/AnimalGrowthApplication.java
@@ -0,0 +1,13 @@
+package com.chinh.AnimalGrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalGrowthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalGrowthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chinh/AnimalGrowth/AnimalGrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinh.AnimalGrowth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalGrowthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
